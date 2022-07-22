8568275cfaff0f7e59f2ffc8ca6eec2d31dda10e
quadify <sdfsd@gmail.com>
2022-07-22 16:53:47 +0200

    grace ready

--- Grace.c
+++ Grace.c
@@ -2,13 +2,5 @@
 /*comment*/
 #define FILEOPEN FILE* file = fopen("Grace_kid.c", "w+");
 #define BAD if (!file){return(1);}
-#define MAIN\
- int main()\
- {\
-  FILEOPEN\
-  BAD\
-  char *str = "#include <stdio.h>%c/*comment*/%c#define FILEOPEN FILE* file = fopen(%cGrace_kid.c%c, %cw+%c);%c#define BAD if (!file){return(1);}%c#define MAIN\\%c int main()\\%c {\\%c  FILEOPEN\\%c  BAD\\%c  char *str = ";\
-  fprintf(file, str, 10, 10, 34, 34, 34, 34, 10, 10, 10, 10, 10, 10, 10, 10);\
- }
-
+#define MAIN int main() {FILEOPEN BAD char *str = "#include <stdio.h>%c/*comment*/%c#define FILEOPEN FILE* file = fopen(%cGrace_kid.c%c, %cw+%c);%c#define BAD if (!file){return(1);}%c#define MAIN int main() {FILEOPEN BAD char *str = %c%s%c; fprintf(file, str, 10, 10, 34, 34, 34, 34, 10, 10, 34, str, 34, 10);}%cMAIN"; fprintf(file, str, 10, 10, 34, 34, 34, 34, 10, 10, 34, str, 34, 10);}
 MAIN
